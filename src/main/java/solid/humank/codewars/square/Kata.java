e2e106b083016c3382569a13e579a0db4fe3a294
kimkao <yikaikao@gmail.com>
2017-02-13 23:59:39 +0800

    complete code with correct logic first, then try to refacor it.

--- src/main/java/solid/humank/codewars/square/Kata.java
+++ src/main/java/solid/humank/codewars/square/Kata.java
@@ -4,17 +4,27 @@ import java.util.Arrays;
 
 /**
  * Created by kim on 2017/2/13.
+ *
+ *
+ * If the number has an integer square root, take this, otherwise square the number.
+ * [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+ *
+ *
  */
 public class Kata {
-    public static double[] squareOrSquareRoot(double[] input) {
+    public static int[] squareOrSquareRoot(int[] input) {
 
         int size = input.length;
-        double[] sutArray = new double[size];
+        int[] sutArray = new int[size];
 
-        for (double item:input) {
-            Math.sqrt(item);
+        for(int i =0; i<input.length;i++){
+
+            if(Math.sqrt((double)input[i]) == (int)Math.sqrt((double)input[i])){
+                sutArray[i] = (int)Math.sqrt((double)input[i]);
+            }else {
+                sutArray[i]=(int)Math.pow(input[i],2);
+            }
         }
-
         return sutArray;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -11,35 +11,19 @@ import static org.junit.Assert.assertEquals;
  */
 public class SolutionTest {
 
-    @Test
-    public void test_while_number_less_than_0(){
-        double[] input = new double[]{-1};
-        double[] expected = new double[]{-1};
-        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
-    }
-
-    @Test
-    public void test_while_number_could_get_square_root(){
-        double[] input = new double[]{4,9};
-        double[] expected = new double[]{2,3};
-        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
-    }
-
-
-
     //CodeWars 官方測試程式, 但我希望能以pure TDD 模式逐步撰寫
-//    @Test
-//    public void basicTests() {
-//        int[] input = new int[] { 4, 3, 9, 7, 2, 1 };
-//        int[] expected = new int[] { 2, 9, 3, 49, 4, 1 };
-//        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
-//
-//        input = new int[] { 100, 101, 5, 5, 1, 1 };
-//        expected = new int[] { 10, 10201, 25, 25, 1, 1 };
-//        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
-//
-//        input = new int[] { 1, 2, 3, 4, 5, 6 };
-//        expected = new int[] { 1, 4, 9, 2, 25, 36 };
-//        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
-//    }
+    @Test
+    public void basicTests() {
+        int[] input = new int[] { 4, 3, 9, 7, 2, 1 };
+        int[] expected = new int[] { 2, 9, 3, 49, 4, 1 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
+
+        input = new int[] { 100, 101, 5, 5, 1, 1 };
+        expected = new int[] { 10, 10201, 25, 25, 1, 1 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
+
+        input = new int[] { 1, 2, 3, 4, 5, 6 };
+        expected = new int[] { 1, 4, 9, 2, 25, 36 };
+        assertEquals(Arrays.toString(expected), Arrays.toString(Kata.squareOrSquareRoot(input)));
+    }
 }
