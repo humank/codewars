497cc32fb8017e0a13a036e0799a7ef16bfc7681
Kim Kao <yikaikao@gmail.com>
2017-02-14 11:47:18 +0800

    refine the method digitCheck

--- src/main/java/solid/humank/codewars/regexp/StringUtils.java
+++ src/main/java/solid/humank/codewars/regexp/StringUtils.java
@@ -3,5 +3,28 @@ package solid.humank.codewars.regexp;
 /**
  * Created by kim on 2017/2/14.
  */
-public class StringUtil {
+public class StringUtils {
+    public static boolean isDigit(String s) {
+
+        boolean isDigit = true;
+
+        if (s.length() > 1) {
+            isDigit = false;
+
+        } else {
+            isDigit = digitCheck(s);
+        }
+
+        return isDigit;
+    }
+
+    private static boolean digitCheck(String s) {
+        try {
+            Integer sut = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException nfe) {
+            System.out.println("The character is not a valid digit.");
+        }
+        return false;
+    }
 }
---
+++ src/test/java/solid/humank/codewars/regexp/README.md
@@ -0,0 +1,4 @@
+The way to determine input value is a digit or not ! 
+Common way is to use de-factor libs, but this time I need to impl it myself. 
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
