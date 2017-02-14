cf7fe67ffb964c3e4445732759df57ab525757e3
Kim Kao <yikaikao@gmail.com>
2017-02-14 16:30:10 +0800

    add the red-light function code.

--- src/main/java/solid/humank/codewars/bitCounting/BitCounting.java
+++ src/main/java/solid/humank/codewars/bitCounting/BitCounting.java
@@ -4,4 +4,7 @@ package solid.humank.codewars.bitCounting;
  * Created by kim on 2017/2/14.
  */
 public class BitCounting {
+    public static int countBits(int i) {
+        return 0;
+    }
 }
--- src/test/java/solid/humank/codewars/bitCounting/BitCountingTest.java
+++ src/test/java/solid/humank/codewars/bitCounting/BitCountingTest.java
@@ -1,7 +1,21 @@
 package solid.humank.codewars.bitCounting;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 /**
  * Created by kim on 2017/2/14.
  */
 public class BitCountingTest {
+
+    @Test
+    public void testGame() {
+        assertEquals(5, BitCounting.countBits(1234));
+        assertEquals(1, BitCounting.countBits(4));
+        assertEquals(3, BitCounting.countBits(7));
+        assertEquals(2, BitCounting.countBits(9));
+        assertEquals(2, BitCounting.countBits(10));
+    }
 }
