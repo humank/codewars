7eba1a65f465c57f02652a40eda3179864b90876
kimkao <yikaikao@gmail.com>
2017-02-17 00:37:56 +0800

    overcome the TODO1

--- src/main/java/solid/humank/codewars/tambadcode/CityClock.java
+++ src/main/java/solid/humank/codewars/tambadcode/CityClock.java
@@ -4,14 +4,17 @@ package solid.humank.codewars.tambadcode;
  * Created by yikai on 2017/2/16.
  */
 public class CityClock {
+    private int utcZeroTime;
+    private int utcOffset;
 
     //TODO 2 CityClock 跟PhoneClock 都有個utcOffset , 可以被一起提取到父類別出來做重構
 
     public CityClock(int utcOffset) {
+        this.utcOffset = utcOffset;
     }
 
 
-    //TODO 1 每次都回傳1 , 這個東西是個壞味道, 應該要改成以變數方式傳遞
+
 
     /**
      * 當地時間 = UTC時間 + 當地時間與UTC時間的時間差
@@ -19,6 +22,13 @@ public class CityClock {
      */
 
     public int getTime() {
-        return 1;
+
+        //TODO 1 每次都回傳1 , 這個東西是個壞味道, 應該要改成以變數方式傳遞
+
+        return this.utcOffset+ this.utcZeroTime;
+    }
+
+    public void setUtcZeroTime(int utcZeroTime) {
+        this.utcZeroTime = utcZeroTime;
     }
 }
--- src/main/java/solid/humank/codewars/tambadcode/PhoneClock.java
+++ src/main/java/solid/humank/codewars/tambadcode/PhoneClock.java
@@ -5,15 +5,21 @@ package solid.humank.codewars.tambadcode;
  */
 public class PhoneClock {
 
+    private CityClock cityClock;
+    private int utcOffset;
+
     //TODO 3 CityClock 跟PhoneClock 都有個utcOffset , 可以被一起提取到父類別出來做重構
     public PhoneClock(int utcOffset) {
+        this.utcOffset = utcOffset;
     }
 
     public void setCityClock(CityClock cityClock) {
 
+        this.cityClock=cityClock;
     }
 
     public void setTime(int time) {
 
+        this.cityClock.setUtcZeroTime(time - this.utcOffset);
     }
 }
