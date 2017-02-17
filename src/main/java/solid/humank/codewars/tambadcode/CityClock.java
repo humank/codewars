4bd92883e8698a3b2b6dda95e1f62ec3025eaf73
kimkao <yikaikao@gmail.com>
2017-02-17 21:47:24 +0800

    refactor the utcOfftime property to super class, and let cityclock/phoneclock to extends it.

--- src/main/java/solid/humank/codewars/tambadcode/CityClock.java
+++ src/main/java/solid/humank/codewars/tambadcode/CityClock.java
@@ -3,14 +3,13 @@ package solid.humank.codewars.tambadcode;
 /**
  * Created by yikai on 2017/2/16.
  */
-public class CityClock {
+public class CityClock extends Clock {
     private int utcZeroTime;
-    private int utcOffset;
 
     //TODO 2 CityClock 跟PhoneClock 都有個utcOffset , 可以被一起提取到父類別出來做重構
 
     public CityClock(int utcOffset) {
-        this.utcOffset = utcOffset;
+        super(utcOffset);
     }
 
 
---
+++ src/main/java/solid/humank/codewars/tambadcode/Clock.java
@@ -0,0 +1,12 @@
+package solid.humank.codewars.tambadcode;
+
+/**
+ * Created by yikai on 17/02/2017.
+ */
+public class Clock {
+    protected int utcOffset;
+
+    public Clock(int utcOffset) {
+        this.utcOffset = utcOffset;
+    }
+}
